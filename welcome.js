be91ab71fd821d2b7fa1b6d493cde0bed8964e07
storagesiddiq <mdsiddiq1204@gmail.com>
2024-07-25 11:53:18 +0530

    hi

--- welcome.js
+++ welcome.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     const startBtn = document.getElementById("startBtn");
     startBtn.addEventListener("click", () => {
-        window.location.href = "index2.html";  // Redirect to the audio player app
+        window.location.href = "index.html";  // Redirect to the audio player app
     });
 });
